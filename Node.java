1a398825c592c8a13c6a2786e34e7fb288bfcfed
Carmel012 <152313246+Carmel012@users.noreply.github.com>
2023-12-04 18:44:05 -0500

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista;
+
+/**
+ *
+ * @author carmelorodriguezchab
+ */
+class Node {
+    int key;
+    Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
